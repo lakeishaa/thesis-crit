35984e901e390a916c2dde939deee89cc956d9c6
lakeisha <sharl281@newschool.edu>
2024-04-13 14:49:02 -0400

    input passwrd

--- main.js
+++ main.js
@@ -232,3 +232,17 @@ h1.addEventListener("click", () => {
   currentIndex = (currentIndex + 1) % texts.length;
   h1.textContent = texts[currentIndex];
 });
+
+
+
+const desc1 = document.getElementById("desc-1");
+const clickx = document.getElementById("clickx");
+desc1.addEventListener("click", function () {
+  const moreInfo = document.querySelector(".more-info");
+  moreInfo.classList.toggle("hide");
+  console.log("cldrithicked");
+});
+clickx.addEventListener("click", function () {
+  const moreInfo = document.querySelector(".more-info");
+  moreInfo.classList.toggle("hide");
+});
